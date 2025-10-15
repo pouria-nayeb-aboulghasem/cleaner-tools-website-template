f5b4afc9c55f36ec6b1f63f05b9a3d47f00abc30
pour68 <pour68@outlook.com>
2025-10-15 20:11:38 +0330

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Introduction
+
+CleanerTools website template with html, css and vanilla javascript.
+
+## Features
+
+- CSS developed by BEM methodology
+- Responsive technique follow mobile-first rules
+- Code foundation is clean and well-structured
+
+## Tools
+
+- HTML
+- CSS
+  - Vanilla
+  - Libraries
+    - RemixIcon
+    - SwiperCSS
+- JavaScript
+  - Vanilla
+  - Libraries
+    - SwiperJS
+    - ScrollRevealJS
+
+## ScreenShot
+
+![ClearTools website preview](/preview.png)
---
+++ assets/js/site.js
@@ -0,0 +1,30 @@
+const navMenu = document.querySelector("#nav-menu"),
+    navClose = document.querySelector("#nav-close"),
+    navToggle = document.querySelector("#nav-toggle");
+
+if (navClose) {
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove("active");
+    });
+}
+
+if (navToggle) {
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.add("active");
+    });
+}
+
+// deactive nav menu after clicking on links
+const navLinks = document.querySelectorAll(".nav__link");
+navLinks.forEach(navLink => {
+    navLink.addEventListener("click", () => {
+        navMenu.classList.remove("active");
+    });
+});
+
+
+// active header
+const header = document.querySelector("#header");
+window.addEventListener("scroll", () => {
+    this.scrollY >= 50 ? header.classList.add("active") : header.classList.remove("active");
+});
