1b3136886d4535dce81bf62e59748e27d611bda4
pour68 <pour68@outlook.com>
2025-10-19 21:06:29 +0330

    overall

--- assets/js/site.js
+++ assets/js/site.js
@@ -29,6 +29,27 @@ window.addEventListener("scroll", () => {
     this.scrollY >= 50 ? header.classList.add("active") : header.classList.remove("active");
 });
 
+// active link
+
+const sections = document.querySelectorAll('[data-section]');
+
+window.addEventListener('scroll', () => {
+    const scrollDown = window.scrollY;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector(`.nav__menu a[href*=${sectionId}]`);
+
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add('active')
+        } else {
+            sectionsClass.classList.remove('active')
+        }
+    })
+});
+
 // swiper
 
 const servicesSwiper = new Swiper('.services__swiper', {
@@ -41,4 +62,12 @@ const servicesSwiper = new Swiper('.services__swiper', {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     }
+});
+
+// scrollup
+
+window.addEventListener("scroll", () => {
+    const scrollup = document.querySelector("#scrollup");
+
+    this.scrollY > 350 ? scrollup.classList.add("active") : scrollup.classList.remove("active");
 });
